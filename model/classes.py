852e5b61d502205404918b9a5d8ac71b7f74a33e
Jathavaan Shankarr <jathavaan12@gmail.com>
2022-03-07 14:17:35 +0100

    refactor: Minor refactors and added some SQL scripts

---
+++ model/classes.py
@@ -0,0 +1,48 @@
+class User:
+    def __init__(self):
+        pass
+
+
+class Country:
+    def __init__(self):
+        pass
+
+
+class Region:
+    def __init__(self):
+        pass
+
+
+class Farm:
+    def __init__(self):
+        pass
+
+
+class CoffeeRoastery:
+    def __init__(self):
+        pass
+
+
+class Species:
+    def __init__(self):
+        pass
+
+
+class RoastedCoffee:
+    def __init__(self):
+        pass
+
+
+class Bean:
+    def __init__(self):
+        pass
+
+
+class ProcessingMethod:
+    def __init__(self):
+        pass
+
+
+class Tasting:
+    def __init__(self):
+        pass
